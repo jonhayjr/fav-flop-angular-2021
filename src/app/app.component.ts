971e96c8feb9af5f0729466cd966b9fc6aea1ffb
jonhayjr <jonhayjr@gmail.com>
2021-12-30 20:18:29 -0800

    Add redirects file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'flop-vote-app';
+
+  favFlop:any[] = [
+    {
+      id: 1,
+      img: '/assets/img/Delgo.jpg',
+      name:'Delgo'
+    },
+    {
+      id: 2,
+      img: '/assets/img/Cinderella.jpeg',
+      name: 'Cinderella'
+    },
+    {
+      id: 3,
+      img: '/assets/img/AGoodDayToDieHard.jpg',
+      name: 'A Good Day To Die Hard'
+    },
+    {
+      id: 4,
+      img: '/assets/img/RestInPeaceDepartment.jpg',
+      name: 'Rest In Peace Department'
+    }
+]
+
+isSelected = -1;
+
+  ngOnInit() {
+
+  }
+
+  voteFor(id:number) {
+    if (this.isSelected === id) {
+      this.isSelected = -1
+    } else {
+      this.isSelected = id;
+    }
+  }
+
+
+  isFlopSelected(id: number) {
+    return this.isSelected === id && this.isSelected !== -1;
+  }
+
+  disableButton(id: number) {
+    return this.isSelected !== id && this.isSelected !== -1;
+  }
+
+  getButtonText(id: number) {
+    return this.isFlopSelected(id) ? 'Unvote' : 'Vote';
+  }
 }
